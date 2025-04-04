6d5a0d928b0501529a43839312d682e2e123f93c
RehanUlHassanAlvi <76509175+RehanUlHassanAlvi@users.noreply.github.com>
2025-04-04 14:54:07 +0500

    Update concernsToBodyParts.js

--- concernsToBodyParts.js
+++ concernsToBodyParts.js
@@ -1,47 +1,237 @@
-module.exports=
-[{concern:"11's"                 ,bodayParts:"Upper Face"                                                                     , treatments:[]},
-{concern:"Acne"                  , bodayParts:"Arms,Back,Chest,Lower Face,Midface,Neck,Upper Face"                              , treatments:[]},
-{concern:"Acne Scarring"         , bodayParts:"Arms,Back,Chest,Lower Face,Midface,Neck,Upper Face"                              , treatments:[]},
-{concern:"Bags Under Eyes"       , bodayParts:"Upper Face"                                                                      , treatments:[]},
-{concern:"Brown Spots"           , bodayParts:"Hands,Lower Face,Midface,Neck,Upper Face"                                        , treatments:[]},
-{concern:"Cherry Angiomas"       , bodayParts:"Chest,Hands,Lower Face,Midface,Upper Face"                                       , treatments:[]},
-{concern:"Clogged Pores"         , bodayParts:"Lower Face,Midface,Neck,Upper Face"                                              , treatments:[]},
-{concern:"Combination Skin"      , bodayParts:"Lower Face,Midface,Neck,Upper Face"                                              , treatments:[]},
-{concern:"Crepey, Aging Skin"    , bodayParts:"Hands,Lower Face,Midface,Neck,Upper Face"                                        , treatments:[]},
-{concern:"Crows Feet"            , bodayParts:"Upper Face"                                                                      , treatments:[]},
-{concern:"Dark Circles"          , bodayParts:"Midface,Upper Face"                                                              , treatments:[]},
-{concern:"Double Chin"           , bodayParts:"Lower Face,Neck"                                                                 , treatments:[]},
-{concern:"Drooping Forehead"     , bodayParts:"Upper Face"                                                                      , treatments:[]},
-{concern:"Dry Skin"              , bodayParts:"Lower Face,Midface,Neck,Upper Face"                                              , treatments:[]},
-{concern:"Excess Fat"            , bodayParts:"Abdomen,Arms,Back,Chest,Lower Face,Midface,Neck,Upper Face"                      , treatments:[]},
-{concern:"Excess Hair"           , bodayParts:"Arms,Back,Buttocks,Chest,Intimate,Lower Face,Lower Legs,Thighs,Upper Face"       , treatments:[]},
-{concern:"Facial Folds"          , bodayParts:"Lower Face,Midface,Upper Face"                                                   , treatments:[]},
-{concern:"Fine Lines"            , bodayParts:"Lower Face,Midface,Neck,Upper Face"                                              , treatments:[]},
-{concern:"Freckles"              , bodayParts:"Lower Face,Midface,Upper Face"                                                   , treatments:[]},
-{concern:"Frown Lines"           , bodayParts:"Lower Face,Midface,Upper Face"                                                   , treatments:[]},
-{concern:"Furrowed Brow"         , bodayParts:"Upper Face"                                                                      , treatments:[]},
-{concern:"Hair"                  , bodayParts:"Midface"                                                                         , treatments:[]},
-{concern:"Hyperpigmentation"     , bodayParts:"Lower Face,Midface,Neck,Upper Face"                                              , treatments:[]},
-{concern:"Jowls Jawline"         , bodayParts:"Lower Face,Neck"                                                                 , treatments:[]},
-{concern:"Large Pores"           , bodayParts:"Lower Face,Midface,Neck,Upper Face"                                              , treatments:[]},
-{concern:"Lip Enhancement"       , bodayParts:"Lips"                                                                            , treatments:[]},
-{concern:"Loose Skin"            , bodayParts:"Arms,Lower Face,Lower Legs,Midface,Neck,Thighs,Upper Face"                       , treatments:[]},
-{concern:"Loss of Facial Volume" , bodayParts:"Lower Face,Midface,Upper Face"                                                   , treatments:[]},
-{concern:"Melasma"               , bodayParts:"Lower Face,Midface,Neck,Upper Face"                                              , treatments:[]},
-{concern:"Muffin Top"            , bodayParts:"Abdomen"                                                                         , treatments:[]},
-{concern:"Nasolabial Folds"      , bodayParts:"Midface"                                                                         , treatments:[]},
-{concern:"Oily Skin"             , bodayParts:"Lower Face,Midface,Neck,Upper Face"                                              , treatments:[]},
-{concern:"Puffy Eyes"            , bodayParts:"Upper Face"                                                                      , treatments:[]},
-{concern:"Red Spots"             , bodayParts:"Hands,Lower Face,Midface"                                                        , treatments:[]},
-{concern:"Rosacea"               , bodayParts:"Lower Face,Midface,Neck,Upper Face"                                              , treatments:[]},
-{concern:"Rough Skin"            , bodayParts:"Lips,Lower Face,Midface,Neck,Upper Face"                                         , treatments:[]},
-{concern:"Sagging Eyelids"       , bodayParts:"Upper Face"                                                                      , treatments:[]},
-{concern:"Sagging Skin"          , bodayParts:"Arms,Chest,Lower Face,Lower Legs,Midface,Neck,Thighs,Upper Face"                 , treatments:[]},
-{concern:"Sensitive Skin"        , bodayParts:"Lips,Lower Face,Midface,Neck,Upper Face"                                         , treatments:[]},
-{concern:"Sunspots"              , bodayParts:"Lower Face,Midface,Upper Face"                                                   , treatments:[]},
-{concern:"Thin Lips"             , bodayParts:"Lips"                                                                            , treatments:[]},
-{concern:"Undefined Cheekbones"  , bodayParts:"Midface"                                                                         , treatments:[]},
-{concern:"Uneven Skin Texture"   , bodayParts:"Lips,Lower Face,Midface,Neck,Upper Face"                                         , treatments:[]},
-{concern:"Uneven Skin Tone"      , bodayParts:"Lips,Lower Face,Midface,Neck,Upper Face"                                         , treatments:[]},
-{concern:"Unwanted Hair"         , bodayParts:"Arms,Back,Chest,Intimate,Lower Face,Lower Legs,Midface,Thighs,Upper Face"        , treatments:[]},
-{concern:"Wrinkles"              , bodayParts:"Arms,Chest,Hands,Lower Face,Midface,Neck,Upper Face"                             , treatments:[]}]
+module.exports= [
+  {
+    "concern": "Acne",
+    "bodyParts": "Arms,Back,Chest,Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Acne Scarring",
+    "bodyParts": "Arms,Back,Chest,Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Bags Under Eyes",
+    "bodyParts": "Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Brown Spots",
+    "bodyParts": "Hands,Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Cherry Angiomas",
+    "bodyParts": "Chest,Hands,Lower Face,Midface,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Clogged Pores",
+    "bodyParts": "Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Combination Skin",
+    "bodyParts": "Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Crepey, Aging Skin",
+    "bodyParts": "Hands,Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Crows Feet",
+    "bodyParts": "Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Dark Circles",
+    "bodyParts": "Midface,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Double Chin",
+    "bodyParts": "Lower Face,Neck",
+    "treatments": []
+  },
+  {
+    "concern": "Drooping Forehead",
+    "bodyParts": "Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Dry Skin",
+    "bodyParts": "Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Excess Fat",
+    "bodyParts": "Abdomen,Arms,Back,Chest,Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Excess Hair",
+    "bodyParts": "Arms,Back,Buttocks,Chest,Intimate,Lower Face,Lower Legs,Thighs,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Facial Folds",
+    "bodyParts": "Lower Face,Midface,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Fine Lines",
+    "bodyParts": "Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Freckles",
+    "bodyParts": "Lower Face,Midface,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Frown Lines",
+    "bodyParts": "Lower Face,Midface,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Furrowed Brow",
+    "bodyParts": "Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Hair",
+    "bodyParts": "Midface",
+    "treatments": []
+  },
+  {
+    "concern": "Hyperpigmentation",
+    "bodyParts": "Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Jowls Jawline",
+    "bodyParts": "Lower Face,Neck",
+    "treatments": []
+  },
+  {
+    "concern": "Large Pores",
+    "bodyParts": "Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Lip Enhancement",
+    "bodyParts": "Lips",
+    "treatments": []
+  },
+  {
+    "concern": "Loose Skin",
+    "bodyParts": "Arms,Lower Face,Lower Legs,Midface,Neck,Thighs,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Loss of Facial Volume",
+    "bodyParts": "Lower Face,Midface,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Melasma",
+    "bodyParts": "Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Muffin Top",
+    "bodyParts": "Abdomen",
+    "treatments": []
+  },
+  {
+    "concern": "Nasolabial Folds",
+    "bodyParts": "Midface",
+    "treatments": []
+  },
+  {
+    "concern": "Oily Skin",
+    "bodyParts": "Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Puffy Eyes",
+    "bodyParts": "Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Red Spots",
+    "bodyParts": "Hands,Lower Face,Midface",
+    "treatments": []
+  },
+  {
+    "concern": "Rosacea",
+    "bodyParts": "Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Rough Skin",
+    "bodyParts": "Lips,Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Sagging Eyelids",
+    "bodyParts": "Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Sagging Skin",
+    "bodyParts": "Arms,Chest,Lower Face,Lower Legs,Midface,Neck,Thighs,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Sensitive Skin",
+    "bodyParts": "Lips,Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Sunspots",
+    "bodyParts": "Lower Face,Midface,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Thin Lips",
+    "bodyParts": "Lips",
+    "treatments": []
+  },
+  {
+    "concern": "Undefined Cheekbones",
+    "bodyParts": "Midface",
+    "treatments": []
+  },
+  {
+    "concern": "Uneven Skin Texture",
+    "bodyParts": "Lips,Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Uneven Skin Tone",
+    "bodyParts": "Lips,Lower Face,Midface,Neck,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Unwanted Hair",
+    "bodyParts": "Arms,Back,Chest,Intimate,Lower Face,Lower Legs,Midface,Thighs,Upper Face",
+    "treatments": []
+  },
+  {
+    "concern": "Excessive Underarm Sweating",
+    "bodyParts": "Underarms",
+    "treatments": []
+  },
+  {
+    "concern": "Underarm Odor",
+    "bodyParts": "Underarms",
+    "treatments": []
+  },
+  {
+    "concern": "Unwanted Tattoos",
+    "bodyParts": "Arms,Back,Chest,Hands,Legs",
+    "treatments": []
+  }
+];
